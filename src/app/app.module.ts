2e4917f5059b101414b5a511cfed8a8e42b34de4
Mustafa Berk Alpay <kisacaberk@gmail.com>
2023-07-24 11:43:02 +0300

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AnaComponent } from './components/ana/ana.component';
+import { AramaKutusuComponent } from './components/arama-kutusu/arama-kutusu.component';
+import { FilmlerComponent } from './components/filmler/filmler.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AnaComponent,
+    AramaKutusuComponent,
+    FilmlerComponent
   ],
   imports: [
     BrowserModule,
